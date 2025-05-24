8d808a04dac3ccf9ca4ed73008d60ff8a169007f
gitdenlev <levchenkoden02@gmail.com>
2025-05-24 20:58:30 +0300

    set spa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import type { NuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
+  ssr: false,
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
