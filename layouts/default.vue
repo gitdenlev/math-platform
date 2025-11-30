40945741693fecf4863ba78068c1a8da9951a19a
gitdenlev <levchenkoden02@gmail.com>
2025-12-01 01:46:03 +0200

    feat suggestion mode & fix problems

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,31 +1,11 @@
 <template>
-  <div class="relative min-h-screen">
-    <!-- Sidebar Trigger Area -->
-    <div
-      class="fixed top-0 left-0 h-full w-6 z-50"
-      @mouseenter="isSidebarOpen = true"
-    ></div>
-
-    <Sidebar :is-open="isSidebarOpen" @close="isSidebarOpen = false" />
-
-    <div
-      class="transition-transform duration-300 ease-in-out"
-      :class="{ 'pl-72': isSidebarOpen }"
-    >
-      <HumanifyHeader />
-      <main class="p-4 sm:p-6 lg:p-8">
-        <slot />
-      </main>
-      <HumanifyFooter />
-    </div>
+  <div class="relative min-h-screen bg-white dark:bg-gray-950">
+    <main class="p-4 sm:p-6 lg:p-8">
+      <slot />
+    </main>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-import Sidebar from "~/components/sidebar/Sidebar.vue";
-import HumanifyHeader from "~/components/HumanifyHeader.vue";
-import HumanifyFooter from "~/components/HumanifyFooter.vue";
-
-const isSidebarOpen = ref(false);
+// Layout logic here if needed
 </script>
