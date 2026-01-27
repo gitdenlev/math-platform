98812abcba3528b6f0a45ebb9a192ba2e5f727f0
gitdenlev <levchenkoden02@gmail.com>
2026-01-28 01:06:04 +0200

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,7 +18,7 @@ export default defineNuxtConfig({
     "@nuxt/image",
     "@nuxt/ui",
     "@nuxtjs/color-mode",
-    "nuxt-vuefire",
+    // "nuxt-vuefire",
     "@nuxtjs/supabase",
   ],
 
@@ -42,18 +42,18 @@ export default defineNuxtConfig({
     },
   },
 
-  vuefire: {
-    config: {
-      apiKey: process.env.NUXT_PUBLIC_FIREBASE_API_KEY,
-      authDomain: process.env.NUXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.NUXT_PUBLIC_FIREBASE_PROJECT_ID,
-      storageBucket: process.env.NUXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.NUXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.NUXT_PUBLIC_FIREBASE_APP_ID,
-      measurementId: process.env.NUXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-    },
-    analytics: true,
-  },
+  // vuefire: {
+  //   config: {
+  //     apiKey: process.env.NUXT_PUBLIC_FIREBASE_API_KEY,
+  //     authDomain: process.env.NUXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  //     projectId: process.env.NUXT_PUBLIC_FIREBASE_PROJECT_ID,
+  //     storageBucket: process.env.NUXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  //     messagingSenderId: process.env.NUXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  //     appId: process.env.NUXT_PUBLIC_FIREBASE_APP_ID,
+  //     measurementId: process.env.NUXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  //   },
+  //   analytics: true,
+  // },
 
   supabase: {
     redirect: false,
